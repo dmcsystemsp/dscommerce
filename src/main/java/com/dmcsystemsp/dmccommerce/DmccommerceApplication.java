098f6c5b87215e74d17dea33311e79777cd54c0f
Daniel Moura Carneiro <daniel@dell5510.localdomain>
2022-09-15 21:56:51 -0300

    Primeira Versão: Order

---
+++ src/main/java/com/dmcsystemsp/dmccommerce/DmccommerceApplication.java
@@ -0,0 +1,14 @@
+package com.dmcsystemsp.dmccommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmccommerceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DmccommerceApplication.class, args);
+	}
+
+}
