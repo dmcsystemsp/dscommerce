5902dc0a007ad71d8e490313aae2e83411d04396
Daniel Carneiro <daniel@dmcsysteminformatica.com.br>
2022-09-22 18:41:16 -0300

    Bean Validation

--- src/main/java/com/dmcsystemsp/dmccommerce/dto/ProductDTO.java
+++ src/main/java/com/dmcsystemsp/dmccommerce/dto/ProductDTO.java
@@ -2,12 +2,18 @@ package com.dmcsystemsp.dmccommerce.dto;
 
 import com.dmcsystemsp.dmccommerce.entities.Product;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min = 3, max = 80, message = "O nome precisa ter de 3 a 80 caracteres!")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10, message = "A descrição deve ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço tem que ser positivo!")
     private Double price;
     private String imgUrl;
 
@@ -22,6 +28,7 @@ public class ProductDTO {
     }
 
     public ProductDTO(Product entity) {
+
         id = entity.getId();
         name = entity.getName();
         description = entity.getDescription();
